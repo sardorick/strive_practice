333a4486f7f73340f937a2db8f89c015497e63c8
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-03-07 17:19:18 +0100

    Update Ex 23, 24.py

--- 06. Pandas/Ex 23, 24.py
+++ 06. Pandas/Ex 23, 24.py
@@ -1,19 +1,19 @@
-# **23.** Given a DataFrame of numeric values, say
-```python
-df = pd.DataFrame(np.random.random(size=(5, 3))) # a 5x3 frame of float values
-```
+# # **23.** Given a DataFrame of numeric values, say
+# ```python
+# df = pd.DataFrame(np.random.random(size=(5, 3))) # a 5x3 frame of float values
+# ```
 
-how do you subtract the row mean from each element in the row?
+# how do you subtract the row mean from each element in the row?
 df = pd.DataFrame(np.random.random(size=(5, 3))) # a 5x3 frame of float values
 df.mean(axis=1)
 
 
-# **24.** Suppose you have DataFrame with 10 columns of real numbers, for example:
+# # **24.** Suppose you have DataFrame with 10 columns of real numbers, for example:
 
-```python
-df = pd.DataFrame(np.random.random(size=(5, 10)), columns=list('abcdefghij'))
-```
-Which column of numbers has the smallest sum? (Find that column's label.)
+# ```python
+# df = pd.DataFrame(np.random.random(size=(5, 10)), columns=list('abcdefghij'))
+# ```
+# Which column of numbers has the smallest sum? (Find that column's label.)
 
 df = pd.DataFrame(np.random.random(size=(5, 10)), columns=list('abcdefghij'))
 df2 =  df[list('abcdefghij')].sum()
