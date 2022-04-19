8154467a1d97c5d1b82ccf84b0a451b6c5d21e99
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-04-19 23:49:53 +0200

    Improved predict function

--- 07. Gradient Boosting and Encoding/predict.py
+++ 07. Gradient Boosting and Encoding/predict.py
@@ -5,13 +5,15 @@ from sklearn.compose import ColumnTransformer
 import numpy as np
 from train import train
 import data_handler as dh
-x_train, x_test, y_train, y_test = dh.get_data(r"C:\Users\Lenovo\OneDrive\Documents\Strive repos\strive_practice\07. Gradient Boosting and Encoding\insurance.csv")
+x_train, x_test, y_train, y_test = dh.get_data("07. Gradient Boosting and Encoding/insurance.csv")
 
 
 model = GradientBoostingRegressor(learning_rate=0.01, max_depth=3, n_estimators=500)
 
 scaler = StandardScaler()
+inputs = None
 def get_input():
+    global inputs
     inputs = []
     age = int(input("How old are you? \n"))
     child = int(input("How many children do you have? \n"))
@@ -21,15 +23,17 @@ def get_input():
     region = int(input("Choose one of the regions: \n southwest: 1\n southeast: 2\n northwest: 3\n northeast: 4\n"))
     inputs.append([0, age, sex, bmi, child, smoke, region])
 
-    inputs = np.array(inputs)
-       
-    # scaled_inputs = scaler.fit(np.array(inputs).reshape(1, -1))
+    return np.array(inputs)
+
+def predict_input(inps):
+    # inps = np.array(inps).reshape(1, -1)   
+    # scaled_inputs = scaler.fit(inps)
     ct = ColumnTransformer( [('ordinal', OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1), [5, 0] )], remainder='passthrough')
-    cted = ct.fit_transform(inputs)
+    cted = ct.fit_transform(inps)
 
     # return cted
     train = model.fit(x_train, y_train)
-    predictions = model.predict(inputs)
+    predictions = model.predict(inps)
     '''
     Preprocess
     predict
@@ -37,4 +41,4 @@ def get_input():
     '''
     return predictions
 
-print(get_input())
+print(predict_input(get_input()))
