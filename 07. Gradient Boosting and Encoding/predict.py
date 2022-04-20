ecf2766b08f4204e5c558691a4856f5e6cf95761
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-04-20 10:34:50 +0200

    Update predict.py

--- 07. Gradient Boosting and Encoding/predict.py
+++ 07. Gradient Boosting and Encoding/predict.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler, OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 import numpy as np
 import data_handler as dh
-from train import x_train, x_test, y_train, y_test
+x_train, x_test, y_train, y_test = dh.get_data(r"C:\Users\Lenovo\OneDrive\Documents\Strive repos\strive_practice\07. Gradient Boosting and Encoding\insurance.csv")
 
 
 model = GradientBoostingRegressor(learning_rate=0.01, max_depth=3, n_estimators=500)
@@ -25,19 +25,14 @@ def get_input():
     return np.array(inputs)
 
 def predict_input(inps):
-    # inps = np.array(inps).reshape(1, -1)   
-    # scaled_inputs = scaler.fit(inps)
-    ct = ColumnTransformer( [('ordinal', OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1), [1,4,5] )], remainder='passthrough')
-    cted = ct.fit_transform(inps)
+    # ct = ColumnTransformer( [('ordinal', OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1), [4,5,6] )], remainder='passthrough')
+    # x_train = ct.fit_transform(x_train)
+    # y_train = ct.transform(y_train)
 
-    # return cted
-    train = model.fit(x_train, y_train)
-    predictions = model.predict(cted)
-    '''
-    Preprocess
-    predict
-    
-    '''
+    model.fit(x_train, y_train)
+    predictions = model.predict(inps)
+    # predict
     return predictions
 
 print(predict_input(get_input()))
+# print(get_input())
