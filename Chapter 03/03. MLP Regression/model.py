3806ccb6c93311989e8cefead8204cebd97eb627
sardorbekzokirov <45316437+sardorick@users.noreply.github.com>
2022-05-27 15:25:42 +0200

    Regression

--- Chapter 03/03. MLP Regression/model.py
+++ Chapter 03/03. MLP Regression/model.py
@@ -1,9 +1,29 @@
+from matplotlib.pyplot import cla
 import torch
 import torch.nn.functional as F
 from torch import nn
 from torchsummary import summary
 
-# YOUR CODE HERE
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_1, hidden_2):
+        super(NeuralNetwork, self).__init__()
+        self.hidden1 = nn.Linear(input_size, hidden_1)
+        self.hidden2 = nn.Linear(hidden_1, hidden_2)
+        self.output = nn.Linear(hidden_2, 1)
+        self.tanh = nn.Tanh()
+    
+    def forward(self, x):
+        layer_1 = self.hidden1(x)
+        act_1 = self.tanh(layer_1)
+        layer_2 = self.hidden2(act_1)
+        act_2 = self.tanh(layer_2)
+        output = self.output(act_2)
+        return output
+
+model = NeuralNetwork(10, 20, 20)
+
+# print(model)
 
 
 
--- Chapter 03/03. MLP Regression/train.py
+++ Chapter 03/03. MLP Regression/train.py
@@ -8,8 +8,23 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
-from model import #import your model here
-
+from model import NeuralNetwork
 
 
 # Remember to validate your model: model.eval .........with torch.no_grad() ......model.train
+model = NeuralNetwork(8, 10, 20)
+
+data = 'Chapter 03/03. MLP Regression/data/turkish_stocks.csv'
+
+x_train, x_test, y_train, y_test = dh.load_data(pth=data)
+
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+criterion = nn.L1Loss()
+
+def torch_fit(model, x_train, x_test, y_train, y_test, num_epochs):
+    train_loss = 0
+    test_loss = 0
+    for epoch in range(num_epochs):
+        
+        optimizer.zero_grad()
+        
